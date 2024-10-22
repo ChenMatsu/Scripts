f8e938aed5f57c7fe64298555437ca159e365f85
ChenMatsu <matuyou0301@gmail.com>
2024-10-22 13:01:23 +0900

    ubuntu: adding ssh server and user creation scripts

---
+++ root-user-creation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+OS=$(uname) # Get the operating system
+UbuntuVersion="22.04.1" # Greater than specified version
+
+# Create a new user with sudo privileges
+if [ "$OS" == "Linux" ]; then
+    if [ "$(lsb_release -rs)" -ge "$UbuntuVersion" ]; then
+        sudo adduser $USER
+        sudo usermod -aG sudo $USER
+
+        echo "Root user has been created"
+
+        su - $USER
+
+        if [ $? -eq 0 ]; then
+            echo "Root user has been created and switched to"
+
+            sudo whoami
+        else
+            echo "Failed to switch to root user"
+        fi
+    fi
+fi
+
