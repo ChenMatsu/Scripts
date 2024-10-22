7e7f5a369f6bd86ddab14f8cca12f18e6f484a31
ChenMatsu <matuyou0301@gmail.com>
2024-10-22 18:53:02 +0900

    hotfix: root-user-creation script version check deleted

--- root-user-creation.sh
+++ root-user-creation.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 OS=$(uname) # Get the operating system
-UbuntuVersion="22.04.1" # Greater than specified version
 
 # Create a new user with sudo privileges
 if [ "$OS" == "Linux" ]; then
-    if [ "$(lsb_release -rs)" -ge "$UbuntuVersion" ]; then
-        sudo adduser $USER
-        sudo usermod -aG sudo $USER
+    sudo adduser $USER
+    sudo usermod -aG sudo $USER
 
-        echo "Root user has been created"
+    echo "Root user has been created"
 
-        su - $USER
+    su - $USER
 
-        if [ $? -eq 0 ]; then
-            echo "Root user has been created and switched to"
+    if [ $? -eq 0 ]; then
+        echo "Root user has been created and switched to"
 
-            sudo whoami
+        sudo whoami
 
-            git clone https://github.com/ChenMatsu/Scripts.git
-        else
-            echo "Failed to switch to root user"
-        fi
+        git clone https://github.com/ChenMatsu/Scripts.git
+    else
+        echo "Failed to switch to root user"
     fi
+else
+    echo "This script is only supported on Linux"
 fi
 
