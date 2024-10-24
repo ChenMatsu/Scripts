d23b7200011c5fe50b1e0e85716bfd86e83ff680
ChenMatsu <matuyou0301@gmail.com>
2024-10-24 19:12:34 +0900

    ubuntu: adding miniconda script

--- miniconda.sh
+++ miniconda.sh
@@ -5,6 +5,7 @@ OS=$(uname)
 if [ "$OS" == "Darwin" ]; then
     curl -s https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o Miniconda3-latest-MacOSX-x86_64.sh
     bash Miniconda3-latest-MacOSX-x86_64.sh
+
     source ~/.zshrc
 elif [ "$OS" == "Linux" ]; then
     curl -s https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o Miniconda3-latest-Linux-x86_64.sh
