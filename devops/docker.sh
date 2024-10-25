1bba6966393c7123c4fa3bac4fbd37dc3351a960
ChenMatsu <matuyou0301@gmail.com>
2024-10-25 14:11:48 +0900

    devops: docker script linux command fixed

--- devops/docker.sh
+++ devops/docker.sh
@@ -11,16 +11,21 @@ if [ "$OS" == "Darwin" ]; then
 elif [ "$OS" == "Linux" ]; then
     # Check if docker is already installed
     if !command -v docker &> /dev/null; then
-        sudo apt-get remove docker docker-engine docker.io containerd runc
+        sudo apt-get remove docker docker-engine  docker.io containerd runc
     fi
 
     sudo apt-get update
-    sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stale"
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io
-    sudo apt-get install docker-compose
+    sudo apt-get install docker-ce docker-ce-lo containerd.io docker-buildx-plugin docker-compose-plugin
 else
     echo "This script is only supported on Darwin and Linux"
 fi
